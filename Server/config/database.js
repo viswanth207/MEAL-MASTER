cf0ec362d967f2bac77801a98da6790ed48c07be
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2024-11-10 18:23:04 +0530

    Update database.js

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
-const express = require("express");
-const app = express();
-const connectDB = async () => {
-await mongoose.connect("mongodb://localhost:27017/mealmaster");
-    
+const mongoose = require("mongoose");
+
+const connectDB = () => {
+     
+    mongoose.connect(
+    "mongodb+srv://praveenudayagiri724:PRAVEEN@cluster0.lvusg.mongodb.net/mealmasterdb?retryWrites=true&w=majority&appName=Cluster0"
+)
+.then(() => {
+    console.log('MongoDB connected successfully');
+})
+.catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
 };
 module.exports = connectDB;
-
-
