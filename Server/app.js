73fbc33a865d2049d99871a768105d26c31e685b
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2024-11-10 20:17:41 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -345,15 +345,4 @@ app.get("/", (req, res) => {
 
 
 module.exports = app;
-
 connectDB()
-    .then(() => {
-        console.log("Connected to database");
-        app.listen(3333, () => {
-            console.log("Server is running on port 3333");
-        });
-    })
-    .catch(err => {
-        console.error("Database not connected: " + err.message);
-        process.exit(1); // Exit process if the database connection fails
-    });
